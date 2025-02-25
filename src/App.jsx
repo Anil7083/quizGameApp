10fe393a4e6ec1eb4dbe313fdc00aeeb7746e25b
Anil7083 <wwwanilabnave2@gmail.com>
2025-02-25 08:48:07 +0530

    first comment

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Quiz from './component/Quiz'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>Quiz Game App </h1>
+      <Quiz/>
+    </>
+  )
+}
+
+export default App
